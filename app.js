d9e2a6a0b3c7ec5d8625b5f5fd0ecf502ee4b28e
Jonas-Kaufmann-hslu <149689826+Jonas-Kaufmann-hslu@users.noreply.github.com>
2023-11-02 15:41:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello HSLU!')
+  res.send(`
+    <html>
+    <head>
+      <style>
+        .main-text {
+          text-align: center;
+          font-size: 32px;
+          color: blue;
+        }
+        .smaller-text {
+          text-align: center;
+          font-size: 14px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="main-text">Hallo du, wie geht es dir?</div>
+      <div class="smaller-text">Hello World!</div>
+    </body>
+    </html>
+  `)
 })
 
 app.get('/port', (req, res) => {
